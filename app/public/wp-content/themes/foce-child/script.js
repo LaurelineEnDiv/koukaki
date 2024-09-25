895d2ed75cadbf6bc2596c3e50b1264f8c65aedc
Laureline Auger <laureline.auger@gmail.com>
2024-09-25 21:49:28 +0200

    maj script animation des titres au scroll

--- app/public/wp-content/themes/foce-child/script.js
+++ app/public/wp-content/themes/foce-child/script.js
@@ -1,35 +1,8 @@
-//Déclencher la transition fade in à l'apparition des sections
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Sélectionner tous les éléments  
-    const sections = document.querySelectorAll('.banner, .story, #studio');
-
-    // Créer un IntersectionObserver
-    const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-        // Ajouter la classe "show" quand l'élément entre dans le viewport
-        entry.target.classList.add("show");
-        }
-        else {
-            // Retirer la classe "show" quand l'élément sort du viewport
-            entry.target.classList.remove('show');
-        }
-    });
-    }, { threshold: 0.1 }); // L'élément est considéré comme visible à 10% de son apparition
-
-    // Appliquer l'observer à chaque section
-    sections.forEach(section => {
-    observer.observe(section);
-    });
-});
-
-
 //Déclencher l'animation des titres à leur apparition 
 
     document.addEventListener("DOMContentLoaded", function () {
-        // Sélectionner tous les éléments h2 
-        const titles = document.querySelectorAll("h2");
+        // Sélectionner tous les titres 
+        const titles = document.querySelectorAll("span.title");
     
         // Créer un IntersectionObserver
         const observer = new IntersectionObserver((entries, observer) => {
@@ -45,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
         }, { threshold: 0.1 }); // L'élément est considéré comme visible à 10% de son apparition
     
-        // Appliquer l'observer à chaque h2
+        // Appliquer l'observer à chaque titre
         titles.forEach(title => {
         observer.observe(title);
         });
