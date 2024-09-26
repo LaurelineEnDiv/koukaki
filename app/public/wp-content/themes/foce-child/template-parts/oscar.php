523c5957da7cff4d89ac0a981a6c955e6e22e9ad
Laureline Auger <laureline.auger@gmail.com>
2024-09-26 10:34:13 +0200

    ajout section Oscars

---
+++ app/public/wp-content/themes/foce-child/template-parts/oscar.php
@@ -0,0 +1,5 @@
+<section id="oscar">
+        <div>
+            <h3 class="txt-oscar">Fleurs d’oranger & chats errants<br>est nominé aux Oscars Short<br>Film Animated de 2022 !</h3>
+        </div>
+    </section>   
